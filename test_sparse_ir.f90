02b31003fcaa629d42b489c17f5e38331a9f2856
h-m0r1 <57163368+h-m0r1@users.noreply.github.com>
2023-04-20 02:34:31 -0400

    test

--- test_sparse_ir.f90
+++ test_sparse_ir.f90
@@ -100,8 +100,9 @@ program main
         write(*,*) "test_fermion2"
         if (lflag_gl) then
             do l = 1, ir_obj%size
-                write(*,*) gl_matsu_d(1,l)
-                write(*,*) gl_tau_d(1,l)
+                !write(*,*) gl_matsu_d(1,l)
+                !write(*,*) gl_tau_d(1,l)
+                write(*,*) gl_matsu_d(1,l) - gl_tau_d(1,l)
             end do
             write(*,*) maxval(abs(gl_matsu_d - gl_tau_d))
             if (maxval(abs(gl_matsu_d - gl_tau_d)) > 1d2*eps) then
