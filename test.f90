d22a6817aefde7739294cf39b9fbef8b508a7f0f
h-m0r1 <57163368+h-m0r1@users.noreply.github.com>
2023-04-26 13:12:10 -0400

    test

---
+++ test.f90
@@ -0,0 +1,133 @@
+program main
+    use sparse_ir
+    use sparse_ir_io
+    use sparse_ir_preset
+    implicit none
+    integer, parameter :: num = 10000000
+
+    call test_efficiency_f(.false., .false., num, 1)
+    call test_efficiency_f(.false., .false., num, 10)
+    call test_efficiency_f(.false., .false., num, 50)
+    call test_efficiency_f(.false., .false., num, 100)
+    call test_efficiency_f(.false., .false., num, 200)
+    call test_efficiency_f(.false., .false., num, 12)
+    call test_efficiency_f(.false., .false., num, 60)
+    call test_efficiency_f(.false., .false., num, 120)
+    !
+    call test_efficiency_f(.false., .true., num, 1)
+    call test_efficiency_f(.false., .true., num, 10)
+    call test_efficiency_f(.false., .true., num, 50)
+    call test_efficiency_f(.false., .true., num, 100)
+    call test_efficiency_f(.false., .true., num, 200)
+    call test_efficiency_f(.false., .true., num, 12)
+    call test_efficiency_f(.false., .true., num, 60)
+    call test_efficiency_f(.false., .true., num, 120)
+
+    contains
+
+    ! fermion
+    subroutine test_efficiency_f(preset, positive_only, lsize_ir)
+        logical, intent(in) :: preset
+        logical, intent(in) :: positive_only
+        integer, intent(in) :: num
+        integer, intent(in) :: lsize_ir
+        type(IR) :: ir_obj
+        integer, parameter :: ndigit = 10, nlambda = 4
+        double precision, parameter :: lambda = 1.d1 ** nlambda
+        double precision, parameter :: wmax = 1.d0
+        double precision :: PI
+
+        double precision, parameter :: beta = lambda/wmax, omega0 = 1.d0/beta
+        double precision, parameter :: eps = 1.d-1**ndigit
+
+        complex(kind(0d0)),allocatable :: giv(:,:), gl_matsu(:, :), gl_tau(:, :), gtau(:, :), &
+            giv_reconst(:, :)
+        double precision,allocatable :: gl_matsu_d(:, :), gl_tau_d(:, :), gtau_d(:, :), &
+        integer n, t, l, i, ix
+        integer:: time_begin_c, time_end_c, CountPerSec, CountMax
+        complex(kind(0d0)), PARAMETER :: cone  = (1.0d0, 0.0d0)
+        complex(kind(0d0)), PARAMETER :: ci  = (0.0d0, 1.0d0)
+        complex(kind(0d0)), PARAMETER :: czero  = (0.0d0, 0.0d0)
+        double precision, PARAMETER :: one = 1.0d0
+        double precision, PARAMETER :: zero = 0.0d0
+
+        PI =4.D0*DATAN(1.D0)
+
+        if (preset) then
+            ir_obj = mk_ir_preset(nlambda, ndigit, beta, positive_only)
+        else
+            open(99, file='ir_nlambda4_ndigit10.dat', status='old')
+            ir_obj = read_ir(99, beta, positive_only)
+            close(99)
+        end if
+
+        if (abs(ir_obj%beta - beta) > 1d-10) then
+            write(*,*) "beta does not match"
+            stop 1
+        end if
+        if (abs(ir_obj%wmax - wmax) > 1d-10) then
+            write(*,*) "wmax does not match"
+            stop 1
+        end if
+
+        allocate(giv(1, ir_obj%nfreq_f))
+        allocate(giv_reconst(1, ir_obj%nfreq_f))
+        allocate(gl_matsu(1, ir_obj%size))
+        allocate(gl_tau(1, ir_obj%size))
+        allocate(gtau_reconst(1, ir_obj%ntau))
+        allocate(gl_matsu_d(1, ir_obj%size))
+        allocate(gl_tau_d(1, ir_obj%size))
+        allocate(gtau_reconst_d(1, ir_obj%ntau))
+
+        ! From Matsubara
+        do n = 1, ir_obj%nfreq_f
+            giv(1, n) = 1.d0/(cmplx(0d0, PI*ir_obj%freq_f(n)/beta, kind(0d0)) - omega0)
+        end do
+
+        write(*,*) "test_efficiency_f"
+        write(*,*) "preset = ", preset
+        write(*,*) "positive_only = ", positive_only
+
+        call system_clock(time_begin_c, CountPerSec, CountMax)
+        call sleep(1)
+
+        call system_clock(time_begin_c)
+        do i = 1, num
+            ix = MOD(i - 1, lsize_ir) + 1
+            giv(ix, n) = 1.d0/(cmplx(0d0, PI*ir_obj%freq_f(n)/beta, kind(0d0)) - omega0)
+            if ((ix == lsize_ir) .OR. (i == num)) THEN
+                if (positive_only) then
+                    call fit_matsubara_f(ir_obj, giv, gl_matsu_d)
+                    call evaluate_tau(ir_obj, gl_matsu_d, gtau_reconst_d)
+                    call fit_tau(ir_obj, gtau_reconst_d, gl_tau_d)
+                    call evaluate_matsubara_f(ir_obj, gl_tau_d, giv_reconst)
+                    sum = sum + SUM(REAL(giv_reconst(1:ix, 1)))
+                    giv(:, :) = czero
+                    gl_matsu_d(:, :) = zero
+                    gtau_reconst_d(:, :) = zero
+                    gl_tau_d(:, :) = zero
+                    giv_reconst(:, :) = czero
+                else
+                    call fit_matsubara_f(ir_obj, giv, gl_matsu)
+                    call evaluate_tau(ir_obj, gl_matsu, gtau_reconst)
+                    call fit_tau(ir_obj, gtau_reconst, gl_tau)
+                    call evaluate_matsubara_f(ir_obj, gl_tau, giv_reconst)
+                    sum = sum + SUM(REAL(giv_reconst(1:ix, 1)))
+                    giv(:, :) = czero
+                    gl_matsu(:, :) = czero
+                    gtau_reconst(:, :) = czero
+                    gl_tau(:, :) = czero
+                    giv_reconst(:, :) = czero
+                end if
+            end if
+        end do
+        call system_clock(time_end_c)
+        write(*,*) "sum =", sum
+        write(*,*) real(time_end_c - time_begin_c)/CountPerSec," sec"
+        call sleep(1)
+
+        deallocate(giv, gtau, gl_matsu, gl_tau, gtau_reconst, giv_reconst)
+        deallocate(gtau_d, gl_matsu_d, gl_tau_d, gtau_reconst_d)
+
+        call finalize_ir(ir_obj)
+    end subroutine
--- time.sh
+++ time.sh
@@ -2,5 +2,7 @@ cp test_sparse_ir.f90 test_sparse_ir1.f90
 cp test_sparse_ir.f90 test_sparse_ir2.f90
 cp time_measurement.f90 time_measurement1.f90
 cp time_measurement.f90 time_measurement2.f90
+cp test.f90 test1.f90
+cp test.f90 test2.f90
 make clean
-make all
+make effi # make all
